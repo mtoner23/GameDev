b5ed6db6ff10f25c40c61eb356112192c73d91c1
Michael Toner <miketoner23@gmail.com>
2018-11-26 02:12:56 -0500

    Testing Pushing from intellij

---
+++ Grid/src/grid/Tile.java
@@ -0,0 +1,33 @@
+package grid;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+public class Tile extends Group {
+
+    public Rectangle ground;
+    public Unit unit;
+    public int test;
+
+    public Tile(int x, int y, int w, int h){
+        ground = new Rectangle(x,y,w,h);
+        ground.setStroke(Color.BLACK);
+        ground.setStrokeType(StrokeType.INSIDE);
+        this.getChildren().add(ground);
+    }
+
+    public void setOcean(){
+        ground.setFill(Color.BLUE);
+    }
+    public void setLand(){
+        ground.setFill(Color.GREEN);
+    }
+    public void addUnit(){
+        unit = new Unit(ground);
+        this.getChildren().addAll(unit);
+    }
+
+
+}
---
+++ Grid/src/grid/Unit.java
@@ -0,0 +1,20 @@
+package grid;
+
+
+import javafx.scene.Group;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+
+
+public class Unit extends Group {
+    public Circle c;
+
+
+    public Unit(Rectangle ground){
+        double circleX = ground.getX() + ground.getWidth()/2;
+        double circleY = ground.getY() + ground.getHeight()/2;
+
+        c = new Circle(circleX,circleY,3,Color.RED);
+        this.getChildren().add(c);
+    }
+}
