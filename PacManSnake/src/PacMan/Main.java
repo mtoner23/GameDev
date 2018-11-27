ae6ad3f2f73d31f2e68e0536b30dfe78dc5b2622
Michael Toner <miketoner23@gmail.com>
2018-11-27 18:12:06 -0500

    Added pellets, and fixed git on new OS

--- PacManSnake/src/PacMan/Main.java
+++ PacManSnake/src/PacMan/Main.java
@@ -83,7 +83,9 @@ public class Main extends Application implements Commons{
 
     public void playSnake(){
         snake.act();
-        grid[snake.head.x / GRID_SIZE][snake.head.y / GRID_SIZE] = 1;
+        if(grid[snake.head.x / GRID_SIZE][snake.head.y / GRID_SIZE] != 2){
+            grid[snake.head.x / GRID_SIZE][snake.head.y / GRID_SIZE] = 1;
+        }
         if(snake.head.x/GRID_SIZE == apple.x/GRID_SIZE && snake.head.y/GRID_SIZE == apple.y/GRID_SIZE){
             apple.move();
             for(Segment s : snake.segments){
@@ -91,6 +93,7 @@ public class Main extends Application implements Commons{
                     apple.move();
                 }
             }
+            grid[apple.x / GRID_SIZE][apple.y / GRID_SIZE] = 2;
             snake.extend();
             appleCnt++;
         }
@@ -110,8 +113,12 @@ public class Main extends Application implements Commons{
                     Rectangle r = new Rectangle(i*GRID_SIZE,j*GRID_SIZE,GRID_SIZE,GRID_SIZE);
                     r.setFill(Color.BLUE);
                     borders.getChildren().add(r);
+                }else if(grid[i][j] == 2) {
+                    Circle c = new Circle(i*GRID_SIZE + GRID_SIZE/2,j*GRID_SIZE + GRID_SIZE/2, GRID_SIZE/3);
+                    c.setFill(Color.YELLOW);
+                    pellets.getChildren().add(c);
                 }else{
-                    Circle c = new Circle(i*GRID_SIZE + GRID_SIZE/2,j*GRID_SIZE + GRID_SIZE/2, GRID_SIZE/4);
+                    Circle c = new Circle(i*GRID_SIZE + GRID_SIZE/2,j*GRID_SIZE + GRID_SIZE/2, GRID_SIZE/6);
                     c.setFill(Color.YELLOW);
                     pellets.getChildren().add(c);
                 }
